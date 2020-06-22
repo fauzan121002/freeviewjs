77ff709b8202924a922497e1211e79c3c5ee2d0f
fauzan121002 <developerfauzan@gmail.com>
2020-06-22 21:34:03 +0700

    doc comment

--- freeview.js
+++ freeview.js
@@ -1,3 +1,6 @@
+/*! FreeviewJS | (c) Muhammad Fauzan | github.com/fauzan121002/freeviewjs/license.md */
+
+// Synchronous Preview
 function previewSync(e, t, r) {
     try {
         if ("string" != typeof e) throw new Error("parameter 0 must be defined as string.");
@@ -7,6 +10,8 @@ function previewSync(e, t, r) {
         console.error(e);
     }
 }
+
+// Asynchronous Preview
 function preview(e, t, r) {
     new Promise((n, o) => {
         "string" != typeof e && o("parameter 0 must be defined as string."), "string" != typeof t && o("parameter 1 must be defined as string."), "number" != typeof r && o("parameter 2 must be defined as integer.");
@@ -18,12 +23,18 @@ function preview(e, t, r) {
             });
     }).catch(console.error);
 }
+
+// Remove Current Preview Photo
 function removePreview(e) {
     document.getElementById(e).src = "";
 }
+
+// Reset Input Type File Value 
 function reset(e) {
     document.getElementById(e).value = "";
 }
+
+// Reset All Input Type File Value 
 function resetAll() {
     document.querySelectorAll("input[type=file]").forEach((e) => {
         e.value = "";
